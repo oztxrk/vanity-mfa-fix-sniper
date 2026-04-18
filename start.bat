8b20cbfba6247906667306c5df891bbaf5ca1933
öztürk <denizberkyildirim00@gmail.com>
2026-04-18 08:51:26 +0400

    Delete start.bat

--- start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Ozturk Sniper
-if not exist node_modules (
-  echo [*] Paketler yukleniyor...
-  npm install --silent 2>nul
-)
-node --dns-result-order=ipv4first --no-warnings --no-deprecation --max-semi-space-size=8 index.js
-pause
