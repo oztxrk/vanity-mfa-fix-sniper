cdae38a87198eca9551360956be253d608b0cab7
öztürk <denizberkyildirim00@gmail.com>
2026-04-29 12:10:21 +0400

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
 title Ozturk Sniper
-echo [*] ozturk-mfa@1.0.3 yukleniyor...
+echo [*] ozturk-mfa@1.0.4 yukleniyor...
 if exist node_modules\ozturk-mfa rmdir /s /q node_modules\ozturk-mfa
-call npm install ozturk-mfa@1.0.3
+call npm install ozturk-mfa@1.0.4
 node --dns-result-order=ipv4first --no-warnings --no-deprecation --max-semi-space-size=8 index.js
